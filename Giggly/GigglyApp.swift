8c0f30c3ac1407a87f0ca47d5292821a70572a14
Leon <ln.schwinkendorf@gmail.com>
2024-03-14 07:36:31 +0100

    Initial Commit

---
+++ Giggly/GigglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigglyApp.swift
+//  Giggly
+//
+//  Created by Leon Nils Schwinkendorf on 14.03.24.
+//
+
+import SwiftUI
+
+@main
+struct GigglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
